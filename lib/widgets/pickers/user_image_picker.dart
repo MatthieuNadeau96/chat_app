420940f9f7d959a6bbb5900b873e7d60a315baf3
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-06-20 18:16:45 -0400

    Adding an Image Picker

---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,41 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class UserImagePicker extends StatefulWidget {
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  File _pickedImage;
+  final picker = ImagePicker();
+
+  Future _pickImage() async {
+    final pickedImage = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      _pickedImage = File(pickedImage.path);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 35,
+          backgroundColor: Colors.grey[300],
+          backgroundImage:
+              _pickedImage != null ? FileImage(_pickedImage) : null,
+        ),
+        FlatButton.icon(
+          textColor: Theme.of(context).primaryColor,
+          icon: Icon(Icons.image),
+          label: Text('Add Image'),
+          onPressed: _pickImage,
+        ),
+      ],
+    );
+  }
+}
